8b7202edf6bb20cd7eabf56c8f2a08210c848135
vishnu-3001 <vishnusai3001@yahoo.com>
2025-04-25 14:16:49 -0400

    form created

--- ap-ui/src/Components/Simulation.jsx
+++ ap-ui/src/Components/Simulation.jsx
@@ -1,38 +1,90 @@
 import classes from './Simulation.module.css'
-import simulations from '../Store/simulations.json'
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useEffect,useState,useActionState } from 'react';
+import DisabilitiesEnum from '../Store/Disabilities';
 export default function Simulation(){
     const {id}= useParams();
-    const simulation=simulations[id];
     const navigate=useNavigate();
+    const[simulationData,setSimulationData]=useState(null);
+    const[generate,setGenerate]=useState(false);
     function handleClick(){
         navigate(`/disability/${id}/description`);
     }
-    return(
-        <div className={classes.simulationContiainer}>
-            <button className={classes.genericButton} onClick={handleClick}>Description</button>
-            <button className={classes.genericButton} onClick={handleClick}>Generate</button>
-            <h2>{simulation.title}</h2>
-            {
-                simulation.sections.map((section,index)=>{
-                    return(
-                        <div key={index}>
-                            <h3>{section.heading}</h3>
-                            {typeof section.content === 'string' && (
-                        <p>{section.content}</p>
-                    )}
-                    {Array.isArray(section.content) && (
-                        <ul>
-                            {section.content.map((item, idx) => (
-                                <li key={idx}>{item}</li>
-                            ))}
-                        </ul>
-                    )}
-                            </div>
-                    )
-                })
+    function handleGenerate(){
+        setGenerate(true);
+    }
+    async function generateConversation(){
+        try{
+            const disability=DisabilitiesEnum[id];
+            const result=await fetch(`http://localhost:8000/api/v1/openai/generate_conversation?disability=${disability}`);
+            if(!result.ok){
+                console.error("Error generating conversation")
             }
-        </div>
+            const response=await result.json();
+            setSimulationData(response);
+        }catch(error){
+            console.error("Error generating conversation",error);
+        }
+    }
+    function handleSubmit(_,formData){
+        const tweak=formData.get('tweak');
+        let errors=[];
+        if(!tweak){
+            errors.push("Please enter your required changes or more inputs for modified simulation");
+        }
+        if(tweak.length>150){
+            errors.push("Please enter a shorter input");
+        }
+        console.log(errors);
+        if(errors.length>0){
+            return {errors,enterdValues:{tweak}}
+        }
+        console.log(tweak);
+        return {errors:null,enterdValues:{tweak}}
+    }
+    useEffect(()=>{
+        generateConversation();
+    },[]);
+    const[formState,formAction,pending]=useActionState(handleSubmit,{errors:null})
+    return(
+        <div>
+        {simulationData ? (
+            <div>Simulation is loading...</div>
+        ) : (
+            <div className={classes.simulationContiainer}>
+                <button className={classes.genericButton} onClick={handleClick}>Description</button>
+                {!generate&&<button className={classes.genericButton} onClick={handleGenerate}>Generate</button>}
+                {generate&&(
+                    <div className={classes.tweakContainer}>
+                        <form className={classes.tweakForm} action={formAction}>
+                            <textarea className={classes.tweakInput} 
+                            placeholder="Please enter your required changes or more inputs for simulation"
+                            id="tweak"
+                            name="tweak"
+                            defaultValue={formState.enterdValues?.tweak}
+                            ></textarea>
+                            {
+                                formState.errors&&formState.errors.map((error,index)=>{
+                                    return(
+                                        <p key={index} className={classes.error}>{error}</p>
+                                    )
+                                })
+                            }
+                            <button className={classes.genericButton} type='submit'>Simulate</button>
+                        </form>
+                    </div>
+                )}
+                <h2>{simulationData?.title}</h2>
+                <h3>{simulationData?.scenario}</h3>
+                {simulationData?.conversation.map((converse, index) => (
+                    <div key={index}>
+                        <p><strong>{converse.speaker}:</strong></p>
+                        <p>{converse.text}</p>
+                    </div>
+                ))}
+            </div>
+        )}
+    </div>
     )
 }
